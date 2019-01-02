f77aaed1b627b858438960c5042c077a9d943b06
Johansson <inste@icloud.com>
2019-01-02 10:11:02 +0200

    Initial Commit

---
+++ BmiApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BmiApp
+//
+//  Created by Artem Karmaz on 1/2/19.
+//  Copyright © 2019 Johansson Group. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BmiApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BmiApp
+//
+//  Created by Artem Karmaz on 1/2/19.
+//  Copyright © 2019 Johansson Group. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
